ade8a69eeee03bddfa9642f77debb633217832df
Nan <ywu120766@gmail.com>
2024-09-16 22:12:35 -0400

    update files

--- js_self/backtoTop.js
+++ js_self/backtoTop.js
@@ -1,18 +1,39 @@
-
+// Back To Top Button
 // get the button
-const backButton = document.getElementById('backtoTop')
+const backButton = document.getElementById('backtoTop');
 // show the button when scrolled down 20px from the top
-window.onscroll = function(){
-    if(document.body.scrollTop >20 || document.documentElement.scrollTop >20){
-        backButton.style.display = 'block';
-    }else{
-        backButton.style.display = None
-    }
-};
+// window.onscroll = function(){
+//     if(document.body.scrollTop >20 || document.documentElement.scrollTop >20){
+//         backButton.style.display = 'block';
+//     }else{
+//         backButton.style.display = 'none';
+//     }
+// };
 // click button
 backButton.onclick = function(){
     window.scrollTo({
         top: 0,
         behavior:'smooth'
     })
+};
+
+
+// Display location
+const city = 'Worcester';
+const state = 'MA';
+
+function displayLocation() {
+    document.getElementById("myloc").innerHTML = 'my location is ' + city + ', ' + state;
+}
+displayLocation();
+
+// Print function
+function greet(){
+    document.write("hello JS")
+    }
+greet();
+
+// get luck words
+function getmessage(){
+    document.getElementById('lucky').textContent = 'best one';
 }
