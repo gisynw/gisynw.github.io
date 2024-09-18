ef14e8cf884920e6ff45e96a35ba2ea88ab11670
YANAN WU <ywu120766@gmail.com>
2024-09-18 17:56:40 -0400

    update files

--- js_self/present.js
+++ js_self/present.js
@@ -5,13 +5,17 @@ var modal = document.getElementById("myModal");
 var images = document.getElementsByClassName("gallery-img");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
+var explanationText = document.getElementById("myModal_caption");
+var h1text = document.getElementById("modal-h1")
 
 // Loop through all gallery images
 for (let i = 0; i < images.length; i++) {
     images[i].onclick = function() {
         modal.style.display = "block";
         modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
+        // captionText.innerHTML = this.alt;
+        // explanationText.innerHTML = this.alt
+        h1text.innerHTML = "More details about " + this.alt
     }
 }
 
