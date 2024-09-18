f7daa7644ee26730a3b8b5de3cc805fd986e5989
Nan <ywu120766@gmail.com>
2024-09-18 09:57:36 -0400

    updatefiles

---
+++ js_self/header_fade_in.js
@@ -0,0 +1,31 @@
+window.onload = function() {
+    // Get headers and subheaders
+    const header1 = document.querySelector(".nav_bar_header1");
+    const header2 = document.querySelector(".nav_bar_header2");
+
+
+    let isHeader1Visible = true; // Start with header1 visible
+ 
+    // Function to toggle between headers
+    function toggleHeaders() {
+        if (isHeader1Visible) {
+            // Show header1, hide header2
+            header1.classList.add("show");
+            header2.classList.remove("show");
+         
+        } else {
+            // Show header2, hide header1
+            header2.classList.add("show");
+            header1.classList.remove("show");
+        }
+
+        // Switch between headers after a delay
+        isHeader1Visible = !isHeader1Visible;
+
+        // Set up the cycle to repeat every 3 seconds (1s fade + 2s visible)
+        setTimeout(toggleHeaders, 4000);
+    }
+
+    // Start the toggle cycle after a short delay
+    toggleHeaders();
+};
