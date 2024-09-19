59731fa37d4be9ce7c2718a3ceba2a6f2b206b57
Nan <ywu120766@gmail.com>
2024-09-18 23:10:23 -0400

    update files

--- js_self/responsive_nav.js
+++ js_self/responsive_nav.js
@@ -1,5 +1,5 @@
 function myFunction(){
-    const myTopnav = document.getElementById('nav_bar');
+    const myTopnav = document.getElementById('nav_bar_ul');
     if (myTopnav.className.includes("responsive")) {
         myTopnav.className = myTopnav.className.replace(" responsive", "");
     } else {
